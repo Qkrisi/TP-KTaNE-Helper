8dfb7b5a475bd83690c344d74644e83ae1a117c5
Qkrisi <qkrisiyt@gmail.com>
2020-06-01 16:28:49 +0200

    Fixed try-catch not working

--- src/CS/TP.cs
+++ src/CS/TP.cs
@@ -10,7 +10,7 @@ public static class TP
     public static TextBox MessageBox;
 
     public static readonly Action Done = () => { try { closedByDone = true; doneAct(ModuleWindow.sendAfterDone); } catch { } };
-    public static Action<bool> doneAct;     //Don not use in creators!
+    public static Action<bool> doneAct;     //Do not use in creators!
 
     public static bool closedByDone = false;
     #endregion
